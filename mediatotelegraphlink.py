1b85ffcf5a8e917fc7be3d38b83f8fe986719fd1
Agunivers7 <herokuone7@gmail.com>
2022-10-22 14:06:51 +0530

    Update mediatotelegraphlink.py

--- mediatotelegraphlink.py
+++ mediatotelegraphlink.py
@@ -9,7 +9,7 @@ import os
 
 teletips=Client(
     "MediaToTelegraphLink",
-    api_id = int(os.environ["API_ID"]),
+    api_id = 10651048,
     api_hash = os.environ["API_HASH"],
     bot_token = os.environ["BOT_TOKEN"]
 )
